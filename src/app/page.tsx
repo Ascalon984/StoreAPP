49140e94f341fb981cdcc5282af6b456e43482a5
Aditya <ascalon984@users.noreply.github.com>
2026-05-07 21:48:21 +0700

    shapes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,20 +62,21 @@ export default async function Home() {
       <div className="sticky top-[72px] z-0">
 
         {/* BACKGROUND N-CURVE CONTAINER */}
-        <div className="absolute top-[-25px] left-0 w-full h-[210px] z-[-1]">
+        {/* Height diturunkan dari 210px ke sekitar 240px-250px untuk mendorong lengkungan lebih ke bawah */}
+        <div className="absolute top-[-25px] left-0 w-full h-[245px] z-[-1]">
           <svg
             className="w-full h-full antialiased"
             viewBox="0 0 100 100"
             preserveAspectRatio="none"
           >
             {/* LAYER 1: Emerald Utama */}
-            {/* Pinggir dinaikkan ke 85 dan Tengah ditarik ke 65 agar lengkungan lebih terasa (N-Shape) */}
-            <path d="M0 0 H100 V85 Q50 65 0 85 Z" fill="#065F46" />
+            {/* Titik pinggir diturunkan ke 90 dan tengah ke 75 agar menyisakan ruang banner di bawahnya */}
+            <path d="M0 0 H100 V90 Q50 75 0 90 Z" fill="#065F46" />
 
-            {/* LAYER 2: Shape N Amber (Parallel Band) */}
-            {/* Mengikuti alur Emerald dengan ketebalan yang pas (selisih 8 unit) */}
+            {/* LAYER 2: Shape N Amber */}
+            {/* Mengikuti alur dengan presisi di area yang lebih rendah */}
             <path
-              d="M0 85 Q50 65 100 85 V93 Q50 73 0 93 Z"
+              d="M0 90 Q50 75 100 90 V97 Q50 82 0 97 Z"
               fill="#D97706"
             />
           </svg>
