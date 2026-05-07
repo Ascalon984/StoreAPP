a95c2e21e13f66eec2133eda0d53b3f21790260d
Aditya <ascalon984@users.noreply.github.com>
2026-05-07 20:35:46 +0700

    undo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -76,6 +76,7 @@ export default async function Home() {
           </svg>
         </div>
 
+
         {/* Konten dinaikkan agar merapat dengan header (menghapus mt-1) */}
         <div>
           <Banner initialBanners={banners} />
